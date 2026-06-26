9f79bbb7ef5fe4ac7b9e21eaadebbf1263ae03b5
Ana Clara <ana.c.silva11022aluno.senai.br>
2026-06-26 14:48:33 -0300

    alterar arquivo

--- js/script.js
+++ js/script.js
@@ -1,71 +1,185 @@
 const descricao = document.querySelector("#descricao");
+
 const valor = document.querySelector("#valor");
+
 const quantidade = document.querySelector("#quantidade");
+
 
 const btnAdicionar = document.querySelector("#btnAdicionar");
 
-const listaProdutos = document.querySelector("#listaProdutos");
+
+const divDados = document.querySelector("#div-dados");
+
+
+
+// ARRAY
 
 let produtos = [];
 
+
+
+
+// BOTÃO
+
 btnAdicionar.addEventListener("click", cadastrarProduto);
 
-function cadastrarProduto() {
+
+
+
+// FUNÇÃO CADASTRAR
+
+function cadastrarProduto(){
+
 
     let produto = {
+
+
         descricao: descricao.value,
+
+
         valor: Number(valor.value),
+
+
         quantidade: Number(quantidade.value)
+
+
     };
 
+
+
     produto.total = produto.valor * produto.quantidade;
+
+
+
     produto.adicional = calcularAdicional(produto.total);
+
+
+
+    // COLOCA NO ARRAY
 
     produtos.push(produto);
 
+
+
     mostrarProdutos();
 
+
+
     descricao.value = "";
+
     valor.value = "";
+
     quantidade.value = "";
+
+
 }
 
-function calcularAdicional(total) {
 
-    if (total <= 3000) {
+
+
+
+// FUNÇÃO DE DECISÃO
+
+function calcularAdicional(total){
+
+
+
+    if(total <= 3000){
+
+
         return "Isento";
-    } else if (total <= 8000) {
-        return "5%";
-    } else if (total <= 12000) {
-        return "10%";
-    } else if (total <= 20000) {
-        return "15%";
-    } else {
-        return "20%";
+
+
     }
 
+
+    else if(total <= 8000){
+
+
+        return "5%";
+
+
+    }
+
+
+    else if(total <= 12000){
+
+
+        return "10%";
+
+
+    }
+
+
+    else if(total <= 20000){
+
+
+        return "15%";
+
+
+    }
+
+
+    else{
+
+
+        return "20%";
+
+
+    }
+
+
+
 }
 
-function mostrarProdutos() {
 
-    listaProdutos.innerHTML = "";
 
-    for (let i = 0; i < produtos.length; i++) {
 
-        listaProdutos.innerHTML += `
-            <hr>
 
-            <p><b>Descrição:</b> ${produtos[i].descricao}</p>
+// MOSTRAR ARRAY NA TELA
 
-            <p><b>Valor Unitário:</b> R$ ${produtos[i].valor.toFixed(2)}</p>
+function mostrarProdutos(){
 
-            <p><b>Quantidade:</b> ${produtos[i].quantidade}</p>
 
-            <p><b>Valor Total:</b> R$ ${produtos[i].total.toFixed(2)}</p>
+    divDados.innerHTML = "";
 
-            <p><b>Valor Adicional:</b> ${produtos[i].adicional}</p>
+
+
+    // ESTRUTURA DE REPETIÇÃO
+
+    for(let i = 0; i < produtos.length; i++){
+
+
+
+        divDados.innerHTML += `
+
+
+        <div class="produto">
+
+
+        <h3>${produtos[i].descricao}</h3>
+
+
+        <p>Valor Unitário: R$ ${produtos[i].valor}</p>
+
+
+        <p>Quantidade: ${produtos[i].quantidade}</p>
+
+
+        <p>Valor Total: R$ ${produtos[i].total}</p>
+
+
+        <p>Valor Adicional: ${produtos[i].adicional}</p>
+
+
+        </div>
+
 
         `;
+
+
     }
+
+
 
 }
